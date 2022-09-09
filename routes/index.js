08543f7d22325655186b2919d7e275db25c486bf
keffri <keffri.w.neal@gmail.com>
2022-09-09 12:36:36 -0700

    form render and functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect('/');
+  console.log(messages);
 });
 
 module.exports = router;
